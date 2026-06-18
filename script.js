071fe8c4339cca7380136ac8ad5ae5d62330eff4
Suma Pal Srabanty <151848563+suma-creator@users.noreply.github.com>
2026-06-18 22:00:22 +0600

    Update script.js

--- script.js
+++ script.js
@@ -4,20 +4,19 @@
 
     let counter = 0;
 
-    // Next button
+
     document.getElementById("next").addEventListener("click", () => {
       counter++;
       if (counter >= images.length) {
-        counter = 0; // loop back to first image
+        counter = 0; 
       }
       slides.style.transform = `translateX(-${counter * 100}vw)`;
     });
 
-    // Previous button
     document.getElementById("prev").addEventListener("click", () => {
       counter--;
       if (counter < 0) {
-        counter = images.length - 1; // loop back to last image
+        counter = images.length - 1; 
       }
       slides.style.transform = `translateX(-${counter * 100}vw)`;
     });
